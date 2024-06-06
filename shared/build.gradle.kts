08a33593d0749cb50f66c9ad184806dd1b09dff9
Richard Woollcott <richard.woollcott@appmilla.co.uk>
2024-06-06 10:59:26 +0100

    Remove commented out dependency

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -59,7 +59,6 @@ android {
     dependencies {
         implementation(libs.androidx.core.ktx)
         implementation(libs.koin.android)
-        // implementation(libs.compose.ui.android)
     }
 }
 
